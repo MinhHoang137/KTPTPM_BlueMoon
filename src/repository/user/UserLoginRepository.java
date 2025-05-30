8da1bbf0a7c2cca818886af7e9712efb2a39ad8c
akari-zero-1 <vuminhtri1104@gmail.com>
2025-05-30 22:08:13 +0700

    Xóa toàn bộ nội dung, giữ lịch sử commit

--- src/repository/user/UserLoginRepository.java
+++
@@ -1,8 +0,0 @@
-package repository.user;
-
-import entity.user.User;
-import java.util.Optional;
-
-public interface UserLoginRepository {
-    Optional<User> login(String username, String password);
-}
