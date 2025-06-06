e1967d3869662b1603403533c8ef97837c21e3c1
2004 <ngvulinhphong@gmail.com>
2025-06-06 22:37:52 +0700

    change household entity

--- src/entity/resident/Household.java
+++ src/entity/resident/Household.java
@@ -1,22 +1,37 @@
 package entity.resident;
 
+
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+
+import java.sql.SQLException;
 
 public class Household {
     private int id;
     private String householdNumber;
     private Date registrationDate;
-    private int ownerId;        // ID của chủ hộ
-    private String ownerName;   // Dùng để hiển thị, không lưu DB
+    private int ownerId;
+    private Resident owner;
+    private String addressNumber;
+    private ArrayList<Resident> members = new ArrayList<>();
 
     public Household() {}
 
-    public Household(String householdNumber, Date registrationDate, int ownerId) {
+    /**
+     * Constructor để tạo mới Household và lưu vào DB luôn
+     * @param householdNumber
+     * @param registrationDate
+     * @param ownerId
+     * @throws SQLException nếu có lỗi khi thao tác DB
+     */
+    public Household(String householdNumber, Date registrationDate, int ownerId)  {
         this.householdNumber = householdNumber;
         this.registrationDate = registrationDate;
         this.ownerId = ownerId;
     }
+
 
     // --- Getters/Setters ---
     public int getId() {
@@ -31,18 +46,19 @@ public class Household {
         return householdNumber;
     }
 
-    public void setHouseholdNumber(String householdNumber) {
+    public void setHouseholdNumber(String householdNumber) throws SQLException {
         this.householdNumber = householdNumber;
+
     }
 
     public Date getRegistrationDate() {
         return registrationDate;
     }
 
-    public void setRegistrationDate(Date registrationDate) {
+    public void setRegistrationDate(Date registrationDate) throws SQLException {
         this.registrationDate = registrationDate;
-    }
 
+    }
     public String getFormattedRegistrationDate() {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         return sdf.format(registrationDate);
@@ -52,15 +68,27 @@ public class Household {
         return ownerId;
     }
 
-    public void setOwnerId(int ownerId) {
+    public void setOwnerId(int ownerId) throws SQLException {
         this.ownerId = ownerId;
-    }
 
-    public String getOwnerName() {
-        return ownerName;
     }
-
-    public void setOwnerName(String ownerName) {
-        this.ownerName = ownerName;
+    public Resident getOwner() {
+        return owner;
+    }
+    public void setOwner(Resident owner) {
+        this.owner = owner;
+    }
+    public String getAddressNumber() {
+        return addressNumber;
+    }
+    public void setAddressNumber(String addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+    public ArrayList<Resident> getMembers() {
+        return members;
+    }
+    public void setMembers(ArrayList<Resident> members) {
+        this.members = members;
     }
 }
+
